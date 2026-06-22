a70e7728b460e12a56cf37bc64dde4ac781a999e
skyfrog0 <skyfrog0@163.com>
2026-06-22 21:23:47 +0800

    bug完善，添加测试脚本

---
+++ test/MyMgExp.js
@@ -0,0 +1,28 @@
+const {exportFromMongo} = require("../scripts/exportFromMongo");
+const {COMPRESSION} = require("../lib/pmtile");
+
+async function main() {
+    const options = {
+        outputPath: 'C:\\Users\\skyfr\\Desktop\\TEMP\\pmt-exp\\s3857gwt.pmtiles',
+        mongoUri: 'mongodb://localhost:27017',
+        dbName: 'vt4_tiles',
+        collectionName: 's3857gw',
+        datasetName: 's3857',
+        concurrency: 10,
+        tileType: 'gvt',
+        tileCompression:  COMPRESSION.NO_COMPRESSION,
+    };
+
+
+    if (!options.outputPath) {
+        console.error("参数不对");
+        process.exit(1);
+    }
+
+    await exportFromMongo(options);
+}
+
+main().catch((err) => {
+    console.error('执行出错了。 Error:', err);
+    process.exit(1);
+});
